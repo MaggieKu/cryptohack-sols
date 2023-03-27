38b5ec159a8d54c77ebbe8d542128a1e07b184f4
Maggie Ku <90940491+MaggieKu@users.noreply.github.com>
2023-03-27 09:48:32 -0500

    Update hex.py

--- intro-to-cryptohack/hex.py
+++ intro-to-cryptohack/hex.py
@@ -1,2 +1,8 @@
+#Hexadecimal can be used in such a way to represent ASCII strings. 
+#First each letter is converted to an ordinal number according to the ASCII table (as in the previous challenge). 
+#Then the decimal numbers are converted to base-16 numbers, otherwise known as hexadecimal. 
+#The numbers can be combined together, into one long hex string.
+
+
 flg = "63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d"
 print(bytes.fromhex(flg))
